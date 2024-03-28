cc4282f7163cc33475c67b09f12e0ef9535fa28b
barrygeedefra <barry.gee@defra.gov.uk>
2024-03-28 16:10:13 +0000

    initial updates

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,18 @@
 const { documents } = require('../models/constants')
+const { getDocuments } = require('../api/documents')
 
 module.exports = {
   method: 'GET',
   path: '/',
   options: {
-    handler: (request, h) => {
-      return h.view('home', { documents })
+    handler: async (request, h) => {
+
+      // this needs to be moved into a controller / service and called from here. the result being passed into the view
+      const documentsData = await getDocuments()
+      const documents = JSON.parse(documentsData.toString())
+      // end
+
+      return h.view('home', { documents }).code(201)
     }
   }
 }
