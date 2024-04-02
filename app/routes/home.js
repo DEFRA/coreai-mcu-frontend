1ca58c85475af6a323185f6eca657e2c9f27d0a0
barrygeedefra <barry.gee@defra.gov.uk>
2024-04-02 10:44:19 +0100

    Add DocumentService

--- app/routes/home.js
+++ app/routes/home.js
@@ -6,7 +6,7 @@ module.exports = {
   options: {
     handler: async (request, h) => {
       const documentsService = new DocumentsService()
-      const documents = await documentsService.getDocuments()
+      const documents = await documentsService.getDocumentsData()
 
       return h.view('home', { documents }).code(201)
     }
--- app/services/documents.js
+++ app/services/documents.js
@@ -1,14 +1,33 @@
+const { format, parseISO } = require('date-fns');
 const { getDocuments } = require('../api/documents')
 
 class DocumentsService {
 
     constructor() {}
-
-    async getDocuments() {
-        try {
-            const documents = await getDocuments()
  
-            return JSON.parse(documents)
+    formatDocumentsData(documentsJSONData) {
+        return documentsJSONData.map(
+            item => {
+                const date = parseISO(item.properties.createdOn)
+                const formattedDate = format(date, 'dd/MM/yyyy')
+
+                return {
+                    ...item,
+                    properties: {
+                        ...item.properties,
+                        createdOn: formattedDate
+                    }
+                }
+            }
+        )
+    }
+
+    async getDocumentsData(id = null) {
+        try {
+            const documents = id ? await getDocuments(id) : await getDocuments()
+            const documentsJSONData = JSON.parse(documents)
+
+            return this.formatDocumentsData(documentsJSONData)
 
         } catch (error) {
             console.error('There was a problem getting documents data', error)
