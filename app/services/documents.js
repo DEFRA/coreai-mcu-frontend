ace4ec1fd64c141f627519b5fe3c102f5f621c94
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-04-02 13:44:04 +0100

    Remove optional param

--- app/services/documents.js
+++ app/services/documents.js
@@ -1,21 +1,21 @@
 const { format, parseISO } = require('date-fns')
 const { getDocuments } = require('../api/documents')
 
-const formatDocumentsData = (documentsJSONData) => {
-  return documentsJSONData.map(
-    item => {
-      const date = parseISO(item.properties.createdOn)
-      const formattedDate = format(date, 'dd/MM/yyyy')
+const formatDocument = (document) => {
+  const date = parseISO(item.properties.createdOn)
+  const formattedDate = format(date, 'dd/MM/yyyy')
 
-      return {
-        ...item,
-        properties: {
-          ...item.properties,
-          createdOn: formattedDate
-        }
-      }
+  return {
+    ...item,
+    properties: {
+      ...item.properties,
+      createdOn: formattedDate
     }
-  )
+  }
+}
+
+const formatDocuments = (documentsJSONData) => {
+  return documentsJSONData.map(formatDocument)
 }
 
 const getDocumentsData = async () => {
@@ -23,7 +23,7 @@ const getDocumentsData = async () => {
     const documents = await getDocuments()
     const documentsJSONData = JSON.parse(documents)
 
-    return formatDocumentsData(documentsJSONData)
+    return formatDocuments(documentsJSONData)
   } catch (error) {
     console.error('There was a problem getting documents data', error)
     throw error
