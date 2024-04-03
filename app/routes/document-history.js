1f3c2e3ff2bd8331c977c3e822e9eb89e95464bb
barrygeedefra <barry.gee@defra.gov.uk>
2024-04-03 10:39:03 +0100

    update router

--- app/routes/document-history.js
+++ app/routes/document-history.js
@@ -1,13 +1,29 @@
-const { document, responses } = require('../models/constants')
+const Joi = require('joi')
+const { getDocumentResponsesHistoryData } = require('../services/responses')
 
 module.exports = {
   method: 'GET',
   path: '/document/{id}/response/history',
   options: {
-    handler: (request, h) => {
-      const documentId = request.params.id
+    validate: {
+      params: Joi.object({
+        id: Joi.string().uuid().required()
+      })
+    },
+    handler: async (request, h) => {
+      try {
+        const responses = await getDocumentResponsesHistoryData(
+          request.params.id
+        )
 
-      return h.view('document-history', { documentId, document, responses })
+        const documentId = request.params.id
+
+        return h.view('document-history', { responses, documentId })
+
+      } catch(error) {
+        console.log('ERROR:', error)
+        return h.view('document-history', { error })
+      }
     }
   }
 }
