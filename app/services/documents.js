7aa7e8f4a4f748b030b48e2d14ac86da6c027f83
barrygeedefra <barry.gee@defra.gov.uk>
2024-04-03 15:19:16 +0100

    Refactor

--- app/services/documents.js
+++ app/services/documents.js
@@ -19,11 +19,32 @@ const formatDocuments = (documents) => {
   return documents.map(formatDocument)
 }
 
+const { getDocuments } = require('../api/documents')
+
+const formatDocumentsData = (documentsJSONData) => {
+  return documentsJSONData.map(
+    item => {
+      const date = parseISO(item.properties.createdOn)
+      const formattedDate = format(date, 'dd/MM/yyyy')
+
+      return {
+        ...item,
+        properties: {
+          ...item.properties,
+          createdOn: formattedDate
+        }
+      }
+    }
+  )
+}
+
 const getDocumentsData = async () => {
   try {
     const documents = await getDocuments()
+    const documentsJSONData = JSON.parse(documents)
 
-    return formatDocuments(documents)
+    return formatDocumentsData(documentsJSONData)
+
   } catch (error) {
     console.error('There was a problem getting documents data', error)
     throw error
