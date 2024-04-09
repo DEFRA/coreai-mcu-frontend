53a8eff2e714ad867bcba4c739f188c9356767bf
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2024-04-09 10:12:04 +0100

    Add landing page and update the routes (#21)
    * Add landing page and update the routes
    
    * Removed console logs
    
    ---------
    
    Co-authored-by: alistairknight <alistair@nts-graphics.co.uk>

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,17 +1,9 @@
-const { getDocumentsData, getDocumentContent } = require('../services/documents')
-
 module.exports = {
   method: 'GET',
   path: '/',
   options: {
     handler: async (request, h) => {
-      const documents = await getDocumentsData()
-
-      for (let i = 0; i < documents.length; i++) {
-        documents[i].contents = await getDocumentContent(documents[i].documentId)
-      }
-
-      return h.view('home', { documents }).code(201)
+      return h.view('home').code(200)
     }
   }
 }
