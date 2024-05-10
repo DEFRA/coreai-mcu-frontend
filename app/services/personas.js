d10a2f496db4b30a6f31fc1123eb528a0f2deff9
BarryGeeDefra <162121761+BarryGeeDefra@users.noreply.github.com>
2024-05-10 10:42:13 +0100

    Refactor personas.js

--- app/services/personas.js
+++ app/services/personas.js
@@ -16,26 +16,3 @@ module.exports = {
   getPersonas,
   getPersona
 }
-
-/*
-const { listPersonas, listPersona } = require('../api/personas')
-
-const getPersonas = async (request, project = 'mcu', type) => {
-  const responses = await listPersonas(project, type)
-
-  return responses
-}
-
-
-const getPersona = async (project = 'mcu', type, name) => {
-  const response = await listPersona(project, type, name)
-
-  return response
-}
-
-module.exports = {
-  getPersonas,
-  getPersona
-}
-
-*/
